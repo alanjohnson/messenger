0c3063ec41f8f98733f9a838e97d5ccc624d6424
skittlyoaten <skittlyoaten@gmail.com>
2025-03-29 13:07:29 -0500

    render users, add click handler

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,25 @@
+import { useContext } from 'react';
 import { StyledUserList } from '@/css/styled_components';
+import { mockUsers } from '@/data/mockData';
+import AppContext from '@/contexts/AppContext';
 
 export default function UserList() {
-  return <StyledUserList>user list here</StyledUserList>;
+  const { recipient } = useContext(AppContext);
+
+  return (
+    <StyledUserList>
+      {mockUsers.map(user => (
+        <button
+          className={recipient === user.name ? 'active' : ''}
+          key={user.id}
+          onClick={() => {
+            console.log('clicky', user.name);
+          }}
+        >
+          <div className="avatar" />
+          <span className="name">{user.name}</span>
+        </button>
+      ))}
+    </StyledUserList>
+  );
 }
